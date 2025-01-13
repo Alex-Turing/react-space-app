c84949a435733131ebb7ff5930b304ea449fe3af
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-01-13 16:51:00 -0500

    Navigation style completed. Mor links to be added

--- src/components/Navbar/NavigationItem/NavigationItem.jsx
+++ src/components/Navbar/NavigationItem/NavigationItem.jsx
@@ -1,12 +1,22 @@
 import styled from "styled-components";
 
+const StyledNavigationItem = styled.li`
+    font-size: 24px;
+    margin-bottom: 30px;
+    line-height: 28px;
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    color: ${props => props.$active ? "#7b78e5" : "#d9d9d9"};
+    font-family: ${props => props.$active ? "GandhiSansBold" : "GandhiSansRegular"};
+`;
 const NavigationItem = (props) => {
     const {children, activeIcon, inactiveIcon, active=false} = props;
     return (
-        <>
+        <StyledNavigationItem $active={active}>
         <img src={active ? activeIcon : inactiveIcon} alt="nav item icon" />
         {children}
-        </>
+        </StyledNavigationItem>
     );
 };
 
