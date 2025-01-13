8e6f87497b8125ff2610ff6bb2d7eba17a7cc498
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-01-13 17:54:11 -0500

    nav menu style completed. Logic to be added

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -13,18 +13,20 @@ const Navbar = () => {
         <aside>
             <nav>
                 <StyledList>
-                    <NavigationItem 
-                        activeIcon="icons/home-active.png" 
-                        inactiveIcon="icons/home-inactive.png" 
-                        active
-                    >
+                    <NavigationItem activeIcon="icons/home-active.png" inactiveIcon="icons/home-inactive.png" active>
                         Home
                     </NavigationItem>
-                    <NavigationItem 
-                        activeIcon="icons/most-views-active.png" 
-                        inactiveIcon="icons/most-views-inactive.png"
-                    >
-                        Most Visited
+                    <NavigationItem activeIcon="icons/most-views-active.png" inactiveIcon="icons/most-views-inactive.png">
+                        Most Viewed
+                    </NavigationItem>
+                    <NavigationItem activeIcon="icons/liked-active.png" inactiveIcon="icons/liked-inactive.png">
+                        Most Liked
+                    </NavigationItem>
+                    <NavigationItem activeIcon="icons/new-active.png" inactiveIcon="icons/new-inactive.png">
+                        New
+                    </NavigationItem>
+                    <NavigationItem activeIcon="icons/sorprise-me-active.png" inactiveIcon="icons/sorprise-me-inactive.png">
+                        Sorprise Me
                     </NavigationItem>
                 </StyledList>
             </nav>
